8920809a8a41a421888ed4778de4002bf8b17d68
Semyon Adamenko <105553489+paniccaaa@users.noreply.github.com>
2024-04-01 19:56:17 +0300

    Update handleUpdatePost.go

--- internal/http-server/handlers/post/handleUpdatePost.go
+++ internal/http-server/handlers/post/handleUpdatePost.go
@@ -15,46 +15,6 @@ import (
 	"github.com/paniccaaa/adamenblog-api/internal/storage/postgres"
 )
 
-// func HandleUpdatePost(log *slog.Logger, storage *postgres.PostgresStore) http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		const op = "handlers.post.HandleUpdatePost"
-
-// 		log := log.With(
-// 			slog.String("op", op),
-// 			slog.String("request_id", middleware.GetReqID(r.Context())),
-// 		)
-
-// 		postID := chi.URLParam(r, "id")
-
-// 		id, err := strconv.Atoi(postID)
-// 		if err != nil {
-// 			log.Error("failed to fetch post id")
-// 		}
-
-// 		post := &postgres.Post{}
-// 		if err := json.NewDecoder(r.Body).Decode(post); err != nil {
-// 			log.Error("failed to decode request body")
-
-// 			render.Status(r, http.StatusBadRequest)
-// 			render.JSON(w, r, res.Error("failed to decode read body"))
-
-// 			return
-// 		}
-
-// 		updatedPost, err := storage.UpdatePost(id, post)
-// 		if err != nil {
-// 			log.Error("failed to update post")
-
-// 			render.Status(r, http.StatusInternalServerError)
-// 			render.JSON(w, r, res.Error("failed to update post"))
-
-// 			return
-// 		}
-
-// 		render.JSON(w, r, updatedPost)
-// 	}
-// }
-
 func HandleUpdatePost(log *slog.Logger, storage *postgres.PostgresStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const op = "handlers.post.HandleUpdatePost"
